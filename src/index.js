5fc1abccecccc3a4c6156621891c5ac7c830aa30
mah385 <mdarifhussain385@gmail.com>
2020-03-03 23:46:29 +0530

    added Google Fonts

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./index.css";
 import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
